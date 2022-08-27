074300bde91e8e48a0f49dc6a2caed75a09a375c
HeftyB <Ashields0923@gmail.com>
2022-08-26 21:16:35 -0400

    initial

---
+++ Set/Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Andrew Shields on 8/26/22.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
